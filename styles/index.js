bf5e64263bf61192e6bb63a49355b390ea2ba105
Renato Alexsander <renato@xbrain.com.br>
2020-05-08 21:20:39 -0300

    Create a global style and add font munito

---
+++ styles/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+  },
+  titleText: {
+    fontSize: 18,
+    fontFamily: "nunito-bold",
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
